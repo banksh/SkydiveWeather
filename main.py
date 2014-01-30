50cadba08df7001ebc1a6f48581179f9283d58be
Banks Hunter <banks@mit.edu>
2014-01-29 21:34:52 -0500

    made things a bit less gross

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import urllib2,datetime
+
 # KORE
 locationName='KORE'
 #obs.lat = '42.57' #+N
@@ -11,47 +13,40 @@ locationID = 2465890
 
 # Get today's date
 now = datetime.datetime.now()
-y = now.year
-m = now.month
-d = now.day
 
-print y, m, d
-
-####### SUNRISE AND SUNSET ####### 
+####### Today's Weather ####### 
+def getCurrentWeather(locationName):
 # Open wunderground.com url
-url_weather = "http://www.wunderground.com/history/airport/" + str(locationName) + "/"+ str(y) + "/" + str(m) + "/" + str(d) + "/DailyHistory.html?theprefset=SHOWMETAR&theprefvalue=1&format=1"
+	url_weather = "http://www.wunderground.com/history/airport/" + str(locationName) + "/"+ str(y) + "/" + str(m) + "/" + str(d) + "/DailyHistory.html?theprefset=SHOWMETAR&theprefvalue=1&format=1"
 
-try: 
-    u_w = urllib2.urlopen(url_weather)
-except urllib2.URLError:
-    end()
-else:
-    data = u_w.read().split('<br />')[1:]
-    data = [s.strip('\n').split(',') for s in data[:-1]]
-    data_weather = [{s[0]:s[1:]} for s in data]
-    
-
-print "Weather"
-print data_weather[-1]
-print
+	try: 
+		u_w = urllib2.urlopen(url_weather)
+	except urllib2.URLError:
+		end()
+	else:
+		data = u_w.read().split('<br />')[1:]
+		data = [s.strip('\n').split(',') for s in data[:-1]]
+		data_weather = [{s[0]:s[1:]} for s in data]
+	return data_weather[-1]
 
 
 ####### SUNRISE AND SUNSET ####### 
 # Get sunrise and sunset times
 #l=2465890;curl -s |sed -n 's/.*sunrise="\([0-9:]\{1,\}\).*="\([0-9:]\{1,\}\).*/\1 \2/p'
 
-url_astro = "http://weather.yahooapis.com/forecastrss?w=" + str(locationID)
-try: 
-    u_a = urllib2.urlopen(url_astro)
-except urllib2.URLError:
-    end()
-else:
-    data = u_a.read().split('<br />')
-    data_astro = [s.strip('\n').split('\n') for s in data]
+def getSunriseSunset(loctionID):
+	url_astro = "http://weather.yahooapis.com/forecastrss?w=" + str(locationID)
+	try: 
+		u_a = urllib2.urlopen(url_astro)
+	except urllib2.URLError:
+		end()
+	else:
+		data = u_a.read().split('<br />')
+		data_astro = [s.strip('\n').split('\n') for s in data]
 
-srise, sset = str([i for i in data_astro[0] if i.startswith('<yweather:astronomy')]).split('"')[1::2]
-
-    
+	#Returns a list of two strings, ['<sunrise>','<sunset>']
+	return str([i for i in data_astro[0] if i.startswith('<yweather:astronomy')]).split('"')[1::2]
+	
 
 exit()
 
