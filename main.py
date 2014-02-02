6737a2652a28b7b4a68b78b7904e20a1edfa2f24
Andrew Horning <ahorning@gmail.com>
2014-02-02 15:31:31 -0500

    now working except for the final print
    Some sloppy coding in places that needs to be tightened, but it works

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ locationID = 2465887 #FYI I was totally testing you, this is Orange, MA
 # Get today's date
 now = datetime.datetime.now()
 
-####### Today's Weather ####### 
+####### Today's Weather #######
+# TimeLocal,TemperatureF,Dew PointF,Humidity,Sea Level PressureIn,VisibilityMPH,Wind Direction,Wind SpeedMPH,Gust SpeedMPH,PrecipitationIn,Events,Conditions,WindDirDegrees,DateUTC
 def getCurrentWeather(locationName, now):
 # Open wunderground.com url
 	url_weather = "http://www.wunderground.com/history/airport/%s/%s/%s/%s/DailyHistory.html?theprefset=SHOWMETAR&theprefvalue=1&format=1" %(str(locationName), str(now.year), str(now.month), str(now.day))
@@ -27,12 +28,14 @@ def getCurrentWeather(locationName, now):
 	else:
 		data = u_w.read().split('<br />')[1:]
 		data = [s.strip('\n').split(',') for s in data[:-1]]
-		data_weather = [{s[0]:s[1:]} for s in data]
-		
+		#data_weather = [{s[0]:s[1:]} for s in data]
+		data_weather = data
+
 	try:
 		data_weather[-1]
 	except IndexError:
-		return "Early"
+		return "It's midnight. Go to sleep."
+		exit()
 		#It's around midnight and there hasn't been a reading yet.
 	else:
 		return data_weather[-1]
@@ -40,7 +43,7 @@ def getCurrentWeather(locationName, now):
 
 ####### SUNRISE AND SUNSET ####### 
 # Get sunrise and sunset times
-#l=2465890;curl -s |sed -n 's/.*sunrise="\([0-9:]\{1,\}\).*="\([0-9:]\{1,\}\).*/\1 \2/p'
+#l=2465887;curl -s |sed -n 's/.*sunrise="\([0-9:]\{1,\}\).*="\([0-9:]\{1,\}\).*/\1 \2/p'
 
 def getSunriseSunset(locationID):
 	url_astro = "http://weather.yahooapis.com/forecastrss?w=" + str(locationID)
@@ -53,19 +56,132 @@ def getSunriseSunset(locationID):
 		data_astro = [s.strip('\n').split('\n') for s in data]
 
 	#Returns a list of two strings, ['<sunrise>','<sunset>']
+<<<<<<< HEAD
 	#Jesus Christ this is like a whole fucking program in itself. 
 	#240 character lines? Ain't nobody got screens fo' that.
 	#Does it seriously add a hardcoded "12"?
 	#The 12 is to convert into 24 hour time if the string originally had 'pm' in it
+=======
+	#Converts to 24 hour clock by adding 12 to pm.
+	#A 240 character line? Ain't nobody got screens fo' that.
+>>>>>>> now working except for the final print
 	return [i.strip(' am') if ' am' in i else ':'.join([str(int(str(i).strip(' pm').split(':')[0])+12),str(i).strip(' pm').split(':')[1]]) for i in str([i for i in data_astro[0] if i.startswith('<yweather:astronomy')]).split('"')[1::2]]	
 
-exit()
-
 ######################## PROCESSING ########################
+# Shove it in a dictionary:
+# TimeLocal,TemperatureF,Dew PointF,Humidity,Sea Level PressureIn,...
+# VisibilityMPH,Wind Direction,Wind SpeedMPH,Gust SpeedMPH,PrecipitationIn,...
+# Events,Conditions,WindDirDegrees,DateUTC
+
+data_weather = getCurrentWeather(locationName, now)
+keyList = ['TimeLocal','TemperatureF','Dew PointF','Humidity','Sea Level PressureIn','VisibilityMPH','Wind Direction','Wind SpeedMPH','Gust SpeedMPH','PrecipitationIn','Events','Conditions','WindDirDegrees','DateUTC']
+allDays_df = {keyList[i]: data_weather[i] for i in range(len(keyList))}
+
 # Daylight
+def daylightTest(datetime_local_string):
 
+	# Get sunrise, sunset
+    sunrise, sunset = getSunriseSunset(locationID)
 
+    format_local_in = '%I:%M %p'
+    
+    # Do the test
+    current_time=datetime.datetime.strptime(datetime_local_string, format_local_in)
+    	
+    format_sun_in = '%H:%M'
+	
+    today_rise_time = datetime.datetime.strptime(sunrise, format_sun_in)
+    today_set_time = datetime.datetime.strptime(sunset, format_sun_in)
+        
+    # It's dark if it's not light
+    if ( today_rise_time <= current_time ) and ( current_time <= today_set_time ):
+        return 'OK'
+    else:
+        return 'Dark'
 
+allDays_df['Daylight']=daylightTest(allDays_df['TimeLocal'])
+
+# Winds
+def windTest(gust_speed):
+    try:
+        float(gust_speed)
+    except ValueError:
+        return 'NAN'
+    else:
+        if float(gust_speed)>20.0:
+            return 'Gusty'
+        else:
+            return 'OK'
+
+if allDays_df['Gust SpeedMPH'] == '-':
+	allDays_df['Gust SpeedMPH'] == float(0.0)
+allDays_df['Winds'] = windTest(allDays_df['Gust SpeedMPH'])
+
+# Temperature
+def tempTest(temp_F):
+    try:
+        float(temp_F)
+    except ValueError:
+        return 'NaN'
+    else:
+        if float(temp_F)<32.0:
+            return 'Freezing Cold'
+        elif float(temp_F)<50.0: #10C = 50F
+            return 'Cold'
+        else:
+            return 'OK'
+
+if allDays_df['TemperatureF'] == '':
+	allDays_df['TemperatureF'] == 'NaN'
+allDays_df['Temperature'] = tempTest(allDays_df['TemperatureF'])
+
+# Clouds
+def cloudTest(conditions):
+    if conditions not in ['Clear', 'Scattered Clouds', 'Partly Cloudy', 'Sunny', 'Mostly Sunny', 'Few Clouds']:
+        return 'Cloudy'
+    else:
+        return 'OK'
+
+allDays_df['Clouds'] = cloudTest(allDays_df['Conditions'])
+
+# Clean up missing values
+# DO THIS FOR DICTIONARY
+#allDays_df= allDays_df.replace('-9999', 'NaN')
+
+# Jumpable Test
+def jumpTest(all_tests):
+    if all_tests['Daylight']=='OK' and all_tests['Winds']=='OK' and all_tests['Temperature']=='OK' and all_tests['Clouds']=='OK':
+        return 'YES'
+    else:
+        return 'NO'
+
+importantValues = {k: allDays_df[k] for k in ['Daylight', 'Winds', 'Temperature', 'Clouds']}
+allDays_df['Jumpable'] = jumpTest(importantValues)
+
+### Final Analysis
+
+def finalPrint(outputConds):
+    return outputConds['Daylight'] + ' ' + outputConds['Winds'] + ' ' + outputConds['Temperature'] + ' ' + outputConds['Clouds'] + ' ' + outputConds['Jumpable']
+
+# THIS IS MESSY SYNTAX AND SLOPPY CODING
+finalDict = {k: allDays_df[k] for k in ['Daylight', 'Winds', 'Temperature', 'Clouds', 'Jumpable']}
+allDays_df['Finalout'] = finalPrint(finalDict)
+
+timeofCheck = allDays_df['TimeLocal']
+jumpingStatus = allDays_df['Finalout'][-1:]
+
+if 'YES' in jumpingStatus:
+    jumpingStatus = list(['Go Jumping!', 'YES'])
+else:
+    for element in jumpingStatus[:-1]:
+        if element=='OK':
+            jumpingStatus.remove(element)
+    
+
+# Output
+# SLOPPY
+print 'At ' + str(locationName) + ' as of ' + timeofCheck + ':'
+print ' '.join(jumpingStatus[:-1])
 
 #### ALL OF THE FOLLOWING IS OLD AND TO BE UPDATED ####
 '''
