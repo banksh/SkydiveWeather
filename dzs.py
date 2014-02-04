d129113245be502528d29c2c34cb5e698ead6a4c
Andrew Horning <ahorning@zeus.(none)>
2014-02-04 09:52:28 -0500

    added more DZs

--- dzs.py
+++ dzs.py
@@ -1,4 +1,34 @@
 IDs ={"KORE":2465887,
 "KSHN":2492259,
 "KASH":12758459,
+"KHBG":2443048,
+"KDEN":2441696,
+"KSFM":2437644,
+"KOMH":2465886,
+"KCGZ":2399130,
+"KVRB":2490414,
+"KRIV":2434852,
+"KOKB":2464118,
 }
+
+## Gold Coast (Lumberton, MS)
+#locationName='KHBG'
+
+## Mile-Hi Skydiving (Longmont)
+#locationName='KDEN'
+
+## Skydive New England
+#locationName='KSFM'
+
+## Skydive Orange, VA
+#locationName='KOMH'
+
+## Eloy / Skydive AZ
+#locationName='KCGZ'
+
+## Skydive Sebastian
+#locationName='KVRB'
+
+## Lake Elsinore CA
+#locationName='KRIV'
+
